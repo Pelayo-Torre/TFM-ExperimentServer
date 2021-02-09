74e387e5dfb203bdc415f6291db027eb40719780
pelayo-torre <pelayogtorre@gmail.com>
2021-02-09 23:04:00 +0100

    Se añade codecov

--- src/main/java/com/uniovi/es/business/experiment/ExperimentServiceImpl.java
+++ src/main/java/com/uniovi/es/business/experiment/ExperimentServiceImpl.java
@@ -5,8 +5,6 @@ import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
-import javax.annotation.PostConstruct;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
